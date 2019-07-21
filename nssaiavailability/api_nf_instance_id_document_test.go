1135f8b021e15abc86551e5b1be92698a72936e9
stevenchiu30801 <steven30801@gmail.com>
2019-07-21 20:49:20 +0800

    test for nssaiavailability store patch api

--- nssaiavailability/api_nf_instance_id_document_test.go
+++ nssaiavailability/api_nf_instance_id_document_test.go
@@ -8,13 +8,14 @@ package nssaiavailability
 
 import (
     "context"
-    // "encoding/json"
+    "encoding/json"
     "net/http"
+    "reflect"
     "testing"
 
     "free5gc-nssf/factory"
     "free5gc-nssf/flog"
-    // . "free5gc-nssf/model"
+    . "free5gc-nssf/model"
     "free5gc-nssf/nssaiavailability/client"
     "free5gc-nssf/test"
 )
@@ -84,7 +85,137 @@ func TestNSSAIAvailabilityDelete(t *testing.T) {
 }
 
 func TestNSSAIAvailabilityPatch(t *testing.T) {
+    factory.InitConfigFactory(testingNssaiavailabilityStoreApi.ConfigFile)
+    if testingNssaiavailabilityStoreApi.MuteLogInd == true {
+        flog.Nssaiavailability.MuteLog()
+        flog.Util.MuteLog()
+    }
 
+    router := NewRouter()
+    srv := &http.Server {
+        Addr: ":8080",
+        Handler: router,
+    }
+
+    go func() {
+        if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+            t.Fatal(err)
+        }
+    }()
+
+    configuration := client.NewConfiguration()
+    configuration.SetBasePath("http://localhost:8080")
+    apiClient := client.NewAPIClient(configuration)
+
+    subtests := []struct {
+        name string
+        nfId string
+        patchItem []PatchItem
+        expectStatus int
+        expectAuthorizedNssaiAvailabilityInfo *AuthorizedNssaiAvailabilityInfo
+    }{
+        {
+            name: "Patch",
+            nfId: "469de254-2fe5-4ca0-8381-af3f500af77c",
+            patchItem: []PatchItem {
+                {
+                    Op: func() PatchOperation { p := PatchOperation_ADD; return p }(),
+                    Path: "/0/supportedSnssaiList/-",
+                    Value: Snssai {
+                        Sst: 1,
+                        Sd: "1",
+                    },
+                },
+                {
+                    Op: func() PatchOperation { p := PatchOperation_COPY; return p }(),
+                    Path: "/1/supportedSnssaiList/0",
+                    From: "/0/supportedSnssaiList/0",
+                },
+            },
+            expectStatus: http.StatusOK,
+            expectAuthorizedNssaiAvailabilityInfo: &AuthorizedNssaiAvailabilityInfo {
+                AuthorizedNssaiAvailabilityData: []AuthorizedNssaiAvailabilityData {
+                    {
+                        Tai: &Tai {
+                            PlmnId: &PlmnId {
+                                Mcc: "466",
+                                Mnc: "92",
+                            },
+                            Tac: "33456",
+                        },
+                        SupportedSnssaiList: []Snssai {
+                            {
+                                Sst: 1,
+                            },
+                            {
+                                Sst: 1,
+                                Sd: "2",
+                            },
+                            {
+                                Sst: 2,
+                            },
+                            {
+                                Sst: 1,
+                                Sd: "1",
+                            },
+                        },
+                    },
+                    {
+                        Tai: &Tai {
+                            PlmnId: &PlmnId {
+                                Mcc: "466",
+                                Mnc: "92",
+                            },
+                            Tac: "33457",
+                        },
+                        SupportedSnssaiList: []Snssai {
+                            {
+                                Sst: 1,
+                            },
+                            {
+                                Sst: 1,
+                                Sd: "1",
+                            },
+                            {
+                                Sst: 1,
+                                Sd: "2",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+    for _, subtest := range subtests {
+        t.Run(subtest.name, func(t *testing.T) {
+            var (
+                resp *http.Response
+                a AuthorizedNssaiAvailabilityInfo
+            )
+
+            a, resp, err := apiClient.NFInstanceIDDocumentApi.NSSAIAvailabilityPatch(context.Background(), subtest.nfId, subtest.patchItem)
+
+            if err != nil {
+                t.Errorf(err.Error())
+            }
+
+            if resp.StatusCode != subtest.expectStatus {
+                t.Errorf("Incorrect status code: expected %d, got %d", subtest.expectStatus, resp.StatusCode)
+            }
+
+            if reflect.DeepEqual(a, *subtest.expectAuthorizedNssaiAvailabilityInfo) == false {
+                e, _ := json.Marshal(subtest.expectAuthorizedNssaiAvailabilityInfo)
+                r, _ := json.Marshal(a)
+                t.Errorf("Incorrect authorized nssai availability info:\nexpected\n%s\n, got\n%s", string(e), string(r))
+            }
+        })
+    }
+
+    err := srv.Shutdown(context.Background())
+    if err != nil {
+        t.Fatal(err)
+    }
 }
 
 func TestNSSAIAvailabilityPut(t *testing.T) {
